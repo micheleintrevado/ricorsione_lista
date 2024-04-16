c7bc251924bd3968ecc3dd9473c1873ff172bb2a
Michele Intrevado <michele.intrevado@student.univaq.it>
2024-04-16 12:47:13 +0200

    small fix

--- lib/impl2.ex
+++ lib/impl2.ex
@@ -1,6 +1,7 @@
 defmodule Impl2 do
-  def do_filter_positive_list(filtered_list, [head | tail]) when is_number(head) and head > 0 do
+  def do_filter_positive_list(filtered_list, [head | tail]) do
     if is_number(head) and head > 0 do
+      IO.puts("--------------> Mi trovo qui")
       filtered_list = [head | filtered_list]
     end
     do_filter_positive_list(filtered_list, tail)
